b809fcb4ed4ca1961045a282cd3b330b150f9000
alex-bq <ale.barrientos@duocuc.cl>
2023-12-11 22:43:11 -0300

    uy ahi estan las rutas

--- MiEstacionamiento/urls.py
+++ MiEstacionamiento/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-#login
+
+# login
 from django.contrib.auth.views import LoginView
-
-
 
 
 urlpatterns = [
@@ -12,5 +11,5 @@ urlpatterns = [
     path("registro/", views.registro, name="registro"),
     path("arrendar/arrendar/", views.guardar_arriendo, name="arrendar_arrendar"),
     path("arrendar/", views.arrendar, name="arrendar"),
-    path('login/', LoginView.as_view(template_name='index.html'), name='login'),
+    path("login/", LoginView.as_view(template_name="index.html"), name="login"),
 ]
